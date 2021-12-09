b81f1eab59ac4cbb5e1ded84f478f6b2fdf1365d
Sylvain MARIE <sylvain.marie@se.com>
2021-12-09 18:04:57 +0100

    Fixed the link in the "just code" example

--- docs/examples/no_output/just_code.py
+++ docs/examples/no_output/just_code.py
@@ -3,9 +3,10 @@
 A short Python script
 =====================
 
-This demonstrates an example ``.py`` file that is not executed when gallery is
-generated (see :ref:`build_pattern`) but nevertheless gets included as an
-example. Note that no output is captured as this file is not executed.
+This demonstrates an example `.py` file that is not executed when gallery is
+generated (see
+[Parsing and executing examples via matching patterns](https://sphinx-gallery.github.io/stable/configuration.html#build-pattern))
+but nevertheless gets included as an example. Note that no output is captured as this file is not executed.
 """
 
 # Code source: Óscar Nájera
