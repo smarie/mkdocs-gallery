48966764b7a43c00908bf93aeb5fe88352390adc
Philippe Marechal - PM219755 <philippe.marechal@cea.fr>
2022-02-25 15:47:32 +0100

    fixed non relevant ValueError raise cases (when project_root_dir is relative - '.' for instance - project_root2 is inevitably different.

--- src/mkdocs_gallery/plugin.py
+++ src/mkdocs_gallery/plugin.py
@@ -225,7 +225,7 @@ markdown_extensions:
         # Store the docs dir relative to project dir
         # (note: same as in AllInformation class but we do not store the whole object)
         project_root_dir = Path(config['config_file_path']).parent
-        self.docs_dir_rel_proj = Path(config['docs_dir']).relative_to(project_root_dir).as_posix()
+        self.docs_dir_rel_proj = Path(config['docs_dir']).relative_to(project_root_dir.resolve()).as_posix()
 
     def on_files(self, files, config):
         """Remove the gallery examples *source* md files (in "examples_dirs") from the built website"""
